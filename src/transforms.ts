92f2bf606377c5546eac2c1e1103b40ae3ea0c01
Denys Hudyria <denys.hudyria@gmail.com>
2023-11-01 23:34:57 +0200

    test t.me

--- src/transforms.ts
+++ src/transforms.ts
@@ -110,7 +110,8 @@ const transforms: transforms = {
         const str = data.source?.split("/"),
           // id_ = str ? `${str[3]}-${str[4]}` : "",
           id = str ? `${str[3]}/${str[4]}` : "";
-        return `<div style="margin: 20px 0; width: 100%;"><script async data-telegram-post="${id}" src="https://telegram.org/js/telegram-widget.js?21" data-width="100%"></script></div>`;
+        // return `<div data-telegram="${id}" style="margin: 20px 0; width: 100%;"><script async data-telegram-post="${id}" src="https://telegram.org/js/telegram-widget.js?21" data-width="100%"></script></div>`;
+        return `<div data-telegram="${id}" style="margin: 20px 0; width: 100%;"></div>`;
       case "twitter":
         return `<iframe width="${data.width}" src="${data.embed}" style="min-height: 630px; max-height: 1000px;" frameborder="0"></iframe>`;
       case "x.com":
